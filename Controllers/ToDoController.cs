3d825936b05bc2fd06db48fdef59df580a5cb1a2
Flora Zhou <zhouruonanlulu@gmail.com>
2021-12-20 01:35:43 +1300

    rename endpoint

--- Controllers/ToDoController.cs
+++ Controllers/ToDoController.cs
@@ -56,7 +56,7 @@ namespace ToDoApi.Controllers
         [ProducesResponseType((int)HttpStatusCode.NotFound)]
         [Route("{id}")]
         [HttpGet]
-        public async Task<ActionResult<ToDo>> GetToDoWithIdAsync([FromRoute]Guid id)
+        public async Task<ActionResult<ToDo>> ToDo([FromRoute]Guid id)
         {
             var toDo = await _context.ToDos.SingleOrDefaultAsync(i => i.Id == id);
             if(toDo == null)
@@ -83,7 +83,7 @@ namespace ToDoApi.Controllers
             ToDo item = new ToDo {Id = Guid.NewGuid(), Title = toDo.Title, State = toDo.State ?? ToDoState.ToDo};
             _context.ToDos.Add(item);
             await _context.SaveChangesAsync();
-            return CreatedAtAction(nameof(GetToDoWithIdAsync), new {id = item.Id}, item);
+            return CreatedAtAction(nameof(ToDo), new {id = item.Id}, item);
         }
 
 
