7d39e144d6f01a92ccaa2c69531aaa854c55f941
Flora Zhou <zhouruonanlulu@gmail.com>
2021-12-20 01:06:50 +1300

    enable swagger

--- Startup.cs
+++ Startup.cs
@@ -78,12 +78,11 @@ namespace ToDoApi
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
-            if (env.IsDevelopment())
-            {
-                app.UseDeveloperExceptionPage();
-                app.UseOpenApi();
-                app.UseSwaggerUi3();
-            }
+
+            app.UseDeveloperExceptionPage();
+            app.UseOpenApi();
+            app.UseSwaggerUi3();
+
 
             app.UseHttpsRedirection();
 
